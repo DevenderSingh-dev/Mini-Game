0c76500ebde6735d0265eb77c850c306bf0c1814
DevenderSingh-dev <devendersingh2139@gmail.com>
2025-09-23 10:12:58 +0530

    script

--- script.js
+++ script.js
@@ -3,25 +3,30 @@ let compsScore = 0;
 
 const choice = document.querySelectorAll(".choice");
 
-const genCompChoie = ()=> {
+const genCompChoice = ()=> {
     const options = ["Rock", "Paper", "Scissors"];
     const randIdx = Math.floor(Math.random() * 3);
     return options[randIdx]
 }
 
+const drawGame = () => {
+    console.log("Game was Draw.")
+}
 const playGame = (userChoice) => {
     console.log("user choice = ", userChoice);
     //generate computer choice
-    const compChoice = genCompChoie();
+    const compChoice = genCompChoice();
     console.log("comp choice = ", compChoice)
     if(userChoice === compChoice) {
         //Draw Game
+        drawGame();
     }
 };
 
 choice.forEach((choice) => {
     choice.addEventListener("click", () => {
-        const Userchoice = choice.getAttribute("id");
-        playGame(Userchoice);
+        const userchoice = choice.getAttribute("id");
+        playGame(userchoice);
     });
 });
+
